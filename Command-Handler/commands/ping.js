324c4a8023167d811c29d54410dc0dbc1d86a8be
Alexander9673 <59809478+Alexander9673@users.noreply.github.com>
2020-05-04 10:21:15 +0800

    use client property instead

--- Command-Handler/commands/ping.js
+++ Command-Handler/commands/ping.js
@@ -1,5 +1,7 @@
 class Ping {
-    constructor () {
+    constructor(client) {
+        Object.defineProperty(this, "client", { value: client });
+        
         this.name = "ping"
         this.aliases = ["clientPing"]
         this.description = "Retrieves bot ping."
@@ -7,9 +9,9 @@ class Ping {
         this.permissions = []
         this.dev = false
     }
-    async run(client, message, args) {
+    async run(message, args) {
         let botping = new Date() - message.createdAt
-        let apiping = client.ws.ping
+        let apiping = this.client.ws.ping
 
         return message.channel.send(`Bot ping: ${botping}ms\nApi ping: ${apiping}ms`)
     }
